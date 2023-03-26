b086561d93e5c15310bae6d2a4deb92c86f5ff26
jansgreen <jansgreen@gmail.com>
2023-03-26 17:41:07 -0400

    cambiado por el mentodo GET 01

--- enviar_email.php
+++ enviar_email.php
@@ -1,5 +1,5 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   // Recibe los datos del formulario
   $nombre = isset($_POST['firstName']) ? $_POST['firstName'] : '';
